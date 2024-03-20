50cc6b573fc75886b1df2a80edea62850025f2ba
Niv <daganniv@gmail.com>
2024-03-20 13:03:58 +0200

    Fixing some input bugs

--- AutomateTheBoringStuffwithPython/8. ReadingAndWritingFiles/regExSearch.py
+++ AutomateTheBoringStuffwithPython/8. ReadingAndWritingFiles/regExSearch.py
@@ -2,8 +2,8 @@ import re
 import os
 
 
-def searchUserInputInTextFilesDirectory(path: str, user_input: str = 'cool'):
-    user_input = str(user_input)
+def searchUserInputInTextFilesDirectory(path: str, user_input: str ):
+
     for fileName in os.listdir(path):
         if os.path.splitext(fileName)[1] == '.txt':
             with open(fileName, 'r') as file:
@@ -14,7 +14,7 @@ def searchUserInputInTextFilesDirectory(path: str, user_input: str = 'cool'):
 
 def main():
     user_input = input("enter a search word: \n")
-    path = str(os.chdir(path=input("enter a Path:\n")))
+    path = input("enter a Path:\n")
     if path == '':
         path = os.getcwd()
     searchUserInputInTextFilesDirectory(path, user_input)
